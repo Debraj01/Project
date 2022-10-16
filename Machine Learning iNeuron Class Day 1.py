68a5330bfeaed75c0286e6c06681951abf2e9e57
Debraj01 <115941631+Debraj01@users.noreply.github.com>
2022-10-16 21:44:10 +0530

    Add files via upload

---
+++ Machine Learning iNeuron Class Day 1.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+1+2
+
+
+# # Intergers
+
+# In[7]:
+
+
+x=10
+x
+
+
+# In[8]:
+
+
+print(x)
+
+
+# In[11]:
+
+
+type(x)
+
+
+# In[25]:
+
+
+a="abc"
+type(a)
+
+
+# In[26]:
+
+
+b=True
+type(b)
+
+
+# In[22]:
+
+
+r=1.3+1j
+r
+###############J is the imaginry number symbol
+
+
+# In[24]:
+
+
+id(r)
+################id give us memory location number
+##############shift+ tab give us detail of that function.
+##############.+tab give us all possible function for that variable
+
+
+# In[27]:
+
+
+True+False
+
+
+# In[28]:
+
+
+False+False
+
+
+# In[29]:
+
+
+a='debraj's'
+
+
+# In[33]:
+
+
+a="debraj's"
+print(a)
+###########If we need to add single quote then we have to use dubble quote then use single quote
+
+
+# # Complex Number
+
+# In[35]:
+
+
+c=5.6+8.3J
+c
+
+
+# In[38]:
+
+
+c.imag
+######Give us imaginary part
+
+
+# In[40]:
+
+
+c.real
+######Give us real part
+
+
+# In[41]:
+
+
+c.conjugate
+
+
+# In[45]:
+
+
+'Debraj'+" "+'Sinha'
+
+
+# In[46]:
+
+
+'Debraj'+1
+
+
+# In[49]:
+
+
+'Debraj'+'1'
+##########Data type must be same
+
+
+# In[50]:
+
+
+'Debraj'+str(1)
+
+
+# # Variables
+# Variables cannot be start from numeric or special character
+
+# In[51]:
+
+
+1="abc"
+
+
+# In[52]:
+
+
+@="deb"
+
+
+# In[54]:
+
+
+abc123="Debraj"
+abc123
+
+
+# # # Boolean
+
+# In[55]:
+
+
+True+True
+
+
+# In[62]:
+
+
+True*True
+
+
+# In[57]:
+
+
+True+False
+
+
+# In[58]:
+
+
+False/True
+
+
+# In[59]:
+
+
+False+False
+
+
+# In[61]:
+
+
+False/False
+
+
+# # IF else Statement
+
+# In[4]:
+
+
+a=5
+if a<3:
+    print("a is less than 3")
+elif a<7:
+    print("a is less than 7")
+else:
+    print("a is gt 7")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
